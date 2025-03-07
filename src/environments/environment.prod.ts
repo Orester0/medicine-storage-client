9df0ebf1d70024ea9aea99634cc9a17672ca8f6b
Orester <110550690+Orester0@users.noreply.github.com>
2025-03-07 15:55:11 +0200

    delete ssr

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://localhost:7215/'
+    apiUrl: 'https://medicine-storage-web-app-d0d0ezcra0heh0ca.polandcentral-01.azurewebsites.net/api/',
+    url: 'https://medicine-storage-web-app-d0d0ezcra0heh0ca.polandcentral-01.azurewebsites.net/'
   };
