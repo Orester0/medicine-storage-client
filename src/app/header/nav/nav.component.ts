ffc7eb3ae5ab54c042960b7fe42ad6ba9956e01b
Orester <110550690+Orester0@users.noreply.github.com>
2025-03-08 14:41:36 +0200

    adaptive navbar

--- src/app/header/nav/nav.component.ts
+++ src/app/header/nav/nav.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, inject, OnInit, HostListener } from '@angular/core';
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { UserPanelComponent } from '../user-panel/user-panel.component';
@@ -13,15 +13,48 @@ import { MatIconModule } from '@angular/material/icon';
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
-export class NavComponent{
+export class NavComponent implements OnInit {
   showAdminDropdown = false;
+  isMenuOpen = false;
   router = inject(Router);
   authService = inject(AuthService);
+  isMobile = false;
 
-  navigateToAdmin(event: Event) {
-    event.preventDefault();
-    this.router.navigate(['/admin']);
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.checkScreenSize();
   }
-  
 
+  ngOnInit() {
+    this.checkScreenSize();
+  }
+
+  private checkScreenSize() {
+    this.isMobile = window.innerWidth <= 768;
+  }
+
+  onDesktop(): boolean {
+    return !this.isMobile;
+  }
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+    if (!this.isMenuOpen) {
+      this.showAdminDropdown = false;
+    }
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+    this.showAdminDropdown = false;
+  }
+
+  toggleAdminDropdown(event: Event): void {
+    event.preventDefault();
+    if (this.isMobile) {
+      this.showAdminDropdown = !this.showAdminDropdown;
+    } else {
+      this.router.navigate(['/admin']);
+    }
+  }
 }
